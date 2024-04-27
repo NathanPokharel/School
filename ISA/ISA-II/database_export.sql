1b00ff54d8a9c542d5fb27f829679b992ba95523
Nathan Pokharel <154725206+NathanPokharel@users.noreply.github.com>
2024-04-27 05:44:42 +0000

    Add files via upload

---
+++ ISA/ISA-II/database_export.sql
@@ -0,0 +1,60 @@
+-- MariaDB dump 10.19-11.3.2-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: WEATHER
+-- ------------------------------------------------------
+-- Server version	11.3.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `weather_data`
+--
+
+DROP TABLE IF EXISTS `weather_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `weather_data` (
+  `city_name` varchar(255) NOT NULL,
+  `temp` int(255) DEFAULT NULL,
+  `humidity` int(255) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `wind_speed` int(255) DEFAULT NULL,
+  `pressure` int(255) DEFAULT NULL,
+  `weather_icon` varchar(10) DEFAULT NULL,
+  `weather_id` int(10) DEFAULT NULL,
+  `last_updated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`city_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weather_data`
+--
+
+LOCK TABLES `weather_data` WRITE;
+/*!40000 ALTER TABLE `weather_data` DISABLE KEYS */;
+INSERT INTO `weather_data` VALUES
+('Kathmandu',27,29,'haze',0,1016,'50d',721,'2024-04-27 04:43:53'),
+('Stockton-on-Tees',-1,79,'scattered clouds',2,1007,'03d',802,'2024-04-27 04:42:40');
+/*!40000 ALTER TABLE `weather_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-27 10:30:34
